bb565df74dea08de1cc590e955911b06df919195
Tushar Shaw <145246175+TUSHAR-SHAWw@users.noreply.github.com>
2024-01-22 12:27:14 +0530

    Initial commit

--- News_Website/settings.py
+++ News_Website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6%*5w*63w@+$!wv-+o1+qptv)2%cpw8jvf$fv^l3!#53vq+hpz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app','now.sh','127.0.0.1','localhost']
 
 
 # Application definition
@@ -74,12 +74,12 @@ WSGI_APPLICATION = 'News_Website.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
